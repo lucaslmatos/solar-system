4a1ebdf5ae46b38c9377285f0039dccbaa5a5d3f
Lucas Matos <lucaslmatos18@gmail.com>
2023-01-24 15:30:44 -0300

    Implementado componente PlanetCard dentro de Solar System - Req 05

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import planets from '../data/planets';
 import Title from './Title';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends Component {
   render() {
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        <span>
+          {planets.map((planet) => (<PlanetCard
+            key={ planet.name }
+            planetName={ planet.name }
+            planetImage={ planet.image }
+          />))}
+        </span>
       </div>
     );
   }
