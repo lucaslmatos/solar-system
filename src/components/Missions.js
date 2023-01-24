0e7d06d241a760f274a0db713b68df851a4510e9
Lucas Matos <lucaslmatos18@gmail.com>
2023-01-24 15:34:28 -0300

    Implementado componente Missions - Req 07

---
+++ src/components/Missions.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+class PlanetCard extends Component {
+  render() {
+    return (
+      <div data-testid="missions"> </div>
+    );
+  }
+}
+
+// PlanetCard.propTypes = {
+//   planetName: PropTypes.string.isRequired,
+//   planetImage: PropTypes.string.isRequired,
+// };
+
+export default PlanetCard;
